da1690571759e0296814e90bb42b0922a88baf1b
Karan Parikh <karan.painkiller@gmail.com>
2014-09-09 21:52:29 -0700

    Off by 3

--- chart2.py
+++ chart2.py
@@ -5,7 +5,7 @@ chart = pygal.Bar(title = "Research papers read per month", x_title = "Month", y
 
 chart.x_labels = "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept"
 
-chart.add("Expected", [3] * 12)
+chart.add("Expected", [3] * 9)
 chart.add("Actual", [1, 1, 0, 0, 0, 0, 2, 2, 1])
 
 chart.render_to_file("chart2.svg") 
